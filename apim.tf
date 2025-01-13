5302843a0d6b4108d677a6a7ca904fda9e30d988
Gabegi <gabriel_pirastru@hotmail.fr>
2025-01-13 14:12:51 +0100

    apim

---
+++ apim.tf
@@ -0,0 +1,9 @@
+resource "azurerm_api_management" "apim" {
+  name                = "apimtesttraining"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  publisher_name      = "My Company"
+  publisher_email     = "company@terraform.io"
+
+  sku_name = "Consumption_0"
+}
