3d46a2ee571c71e49d4ddd235d63abfc6896fc8a
Gabegi <gabriel_pirastru@hotmail.fr>
2025-01-14 14:18:13 +0100

    fixing names

--- api.tf
+++ api.tf
@@ -1,18 +1,4 @@
-# # api from online spec
-# resource "azurerm_api_management_api" "api1" {
-#   name                = "api1"
-#   resource_group_name = azurerm_resource_group.rg.name
-#   api_management_name = azurerm_api_management.apim.name
-#   revision            = "1"
-#   display_name        = "API1"
-#   path                = "api"
-#   protocols           = ["https"]
-#   import {
-#     content_format = "swagger-link-json"
-#     content_value  = file(var.test-api-json)
-#   }
-# }
-
+# api from file
 resource "azurerm_api_management_api" "api1" {
   name                = "example-api1"
   resource_group_name = azurerm_resource_group.rg.name
@@ -28,4 +14,4 @@ resource "azurerm_api_management_api" "api1" {
   }
 }
 
-# api from file
+
