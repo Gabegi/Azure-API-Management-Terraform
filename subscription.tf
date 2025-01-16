2ed8cc5745237ff6599018360b56b3e8868dc3e6
Gabegi <gabriel_pirastru@hotmail.fr>
2025-01-16 15:14:06 +0100

    add sub

--- subscription.tf
+++ subscription.tf
@@ -1,7 +1,8 @@
 resource "azurerm_api_management_subscription" "sub" {
   api_management_name = azurerm_api_management.apim.name
   resource_group_name = azurerm_api_management.apim.resource_group_name
-  user_id             = data.azurerm_api_management_user.example.id
-  product_id          = data.azurerm_api_management_product.example.id
-  display_name        = "Parser API"
+
+  display_name        = "Example API Sub"
+
+  api_id = azurerm_api_management_api.api1.id
 }
