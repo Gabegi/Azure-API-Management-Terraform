38e6f6c700b8d9fcc8b7de4bc010bf117e8ff405
Gabegi <gabriel_pirastru@hotmail.fr>
2025-01-17 16:20:33 +0100

    added managed identity to access kv

--- src/kv.tf
+++ src/kv.tf
@@ -9,7 +9,7 @@ resource "azurerm_key_vault" "kv" {
 resource "azurerm_key_vault_access_policy" "access_policy" {
   key_vault_id = azurerm_key_vault.kv.id
   tenant_id    = data.azurerm_client_config.current.tenant_id
-  object_id    = azurerm_api_management.apim.identity[0].principal_id
+  object_id = data.azurerm_client_config.current.object_id
 
   secret_permissions = ["Get"]
 }
