1e9f4422ebb8916c412b18b67b123ef0d7a894c7
Gabegi <gabriel_pirastru@hotmail.fr>
2025-01-17 15:41:38 +0100

    added versioning

--- src/api.tf
+++ src/api.tf
@@ -8,10 +8,23 @@ resource "azurerm_api_management_api" "api1" {
   path                = "example"
   protocols           = ["https"]
 
+  // versioning
+  version_set_id      = azurerm_api_management_api_version_set.example_version_set.id
+  version             = "v1"
+
   import {
     content_format = "openapi"
     content_value  = file("./api_specs/api1.yaml")
   }
+}
+
+// groups different versions of the same API under a single base path.
+resource "azurerm_api_management_api_version_set" "example_version_set" {
+  name                = "example-api-version-set"
+  resource_group_name = azurerm_resource_group.rg.name
+  api_management_name = azurerm_api_management.apim.name
+  display_name        = "Example API Version Set"
+  versioning_scheme   = "Segment" # Options: Segment, Header, Query
 }
 
 
