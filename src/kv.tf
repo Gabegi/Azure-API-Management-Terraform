313b117967ccd8267e4b43a4c69cc2033cb003c6
Gabegi <gabriel_pirastru@hotmail.fr>
2025-01-20 09:58:14 +0100

    added named value

--- src/kv.tf
+++ src/kv.tf
@@ -6,10 +6,24 @@ resource "azurerm_key_vault" "kv" {
   sku_name = "standard"
 }
 
+resource "azurerm_key_vault_secret" "example_secret" {
+  name         = "my-secret-name"
+  value        = "my-super-secret-value"
+  key_vault_id = azurerm_key_vault.kv.id
+
+  // Optional attributes
+  content_type = "example-content-type"  # E.g., "password", "API key", etc.
+  tags = {
+    environment = "dev"
+  }
+
+  depends_on = [ azurerm_key_vault_access_policy.access_policy ]
+}
+
 resource "azurerm_key_vault_access_policy" "access_policy" {
   key_vault_id = azurerm_key_vault.kv.id
   tenant_id    = data.azurerm_client_config.current.tenant_id
   object_id = data.azurerm_client_config.current.object_id
 
-  secret_permissions = ["Get"]
+  secret_permissions = ["Get", "Set"]
 }
