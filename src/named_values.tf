2fb5ad7b79c5f6a137106bb18b2213877a1e7308
Gabegi <gabriel_pirastru@hotmail.fr>
2025-01-20 11:15:30 +0100

    added nv from kv

--- src/named_values.tf
+++ src/named_values.tf
@@ -9,15 +9,15 @@ resource "azurerm_api_management_named_value" "nv-api-key" {
 }
 
 // secret from key vault
-# resource "azurerm_api_management_named_value" "nv-kv-api-key" {
-#   name                = "kv-api-key"
-#   resource_group_name = azurerm_resource_group.rg.name
-#   api_management_name = azurerm_api_management.apim.name
-#   display_name        = "kv-secret-api-key"
+resource "azurerm_api_management_named_value" "nv-kv-api-key" {
+  name                = "kv-api-key"
+  resource_group_name = azurerm_resource_group.rg.name
+  api_management_name = azurerm_api_management.apim.name
+  display_name        = "kv-secret-api-key"
 
-#   secret              = true
-#   value_from_key_vault {
-#           secret_id = azurerm_key_vault_secret.example_secret.id
-#   }
-# }
+  secret              = true
+  value_from_key_vault {
+          secret_id = azurerm_key_vault_secret.example_secret.id
+  }
+}
 
