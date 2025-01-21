f7031b3bde84e9553afb9c579a242e2eac2ad9e4
Gabegi <gabriel_pirastru@hotmail.fr>
2025-01-21 15:37:20 +0100

    added sa

--- src/backend.tf
+++ src/backend.tf
@@ -17,13 +17,13 @@ resource "azurerm_api_management_backend" "backend" {
 
 
 // Function App that serves as backend
-# resource "azurerm_storage_account" "sa" {
-#   name                     = "kadmafdsafasfdasdfa"
-#   resource_group_name      = azurerm_resource_group.rg.name
-#   location                 = azurerm_resource_group.rg.location
-#   account_tier             = "Standard"
-#   account_replication_type = "LRS"
-# }
+resource "azurerm_storage_account" "sa" {
+  name                     = "kadmafdsafasfdasdfa"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
 
 resource "azurerm_service_plan" "asp" {
   name                = "omg-what-an-asp"
