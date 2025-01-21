518bd97270cd4bcbb666fe6b11f31b30e14fe7e7
Gabegi <gabriel_pirastru@hotmail.fr>
2025-01-21 16:43:24 +0100

    working on policies

--- src/policies.tf
+++ src/policies.tf
@@ -1,7 +1,7 @@
-resource "azurerm_api_management_api_policy" "example" {
-  api_name            = data.azurerm_api_management_api.example.name
-  api_management_name = data.azurerm_api_management_api.example.api_management_name
-  resource_group_name = data.azurerm_api_management_api.example.resource_group_name
+resource "azurerm_api_management_api_policy" "policy" {
+  api_name            = data.azurerm_api_management_api.api1.name
+  api_management_name = azurerm_api_management.apim.name
+  resource_group_name = azurerm_resource_group.rg.name
 
   xml_content = <<XML
 <policies>
