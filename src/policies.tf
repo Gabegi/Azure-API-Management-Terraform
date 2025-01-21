018447831a332050e1ce945332528ecbf9e8d087
Gabegi <gabriel_pirastru@hotmail.fr>
2025-01-21 16:44:18 +0100

    working on policies

--- src/policies.tf
+++ src/policies.tf
@@ -3,11 +3,5 @@ resource "azurerm_api_management_api_policy" "policy" {
   api_management_name = azurerm_api_management.apim.name
   resource_group_name = azurerm_resource_group.rg.name
 
-  xml_content = <<XML
-<policies>
-  <inbound>
-    <find-and-replace from="xyz" to="abc" />
-  </inbound>
-</policies>
-XML
+  xml_content = file("./policies/policy1.xml")
 }
