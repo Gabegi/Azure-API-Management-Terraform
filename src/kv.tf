2fce142fef4d98e8f526b328f22dc5bc96a45650
Gabegi <gabriel_pirastru@hotmail.fr>
2025-01-22 10:08:52 +0100

    working on policies

--- src/kv.tf
+++ src/kv.tf
@@ -1,39 +1,39 @@
-resource "azurerm_key_vault" "kv" {
-  name                = "my-amazing-keyvault"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  tenant_id           = data.azurerm_client_config.current.tenant_id
-  sku_name = "standard"
-}
+# resource "azurerm_key_vault" "kv" {
+#   name                = "my-amazing-keyvault"
+#   location            = azurerm_resource_group.rg.location
+#   resource_group_name = azurerm_resource_group.rg.name
+#   tenant_id           = data.azurerm_client_config.current.tenant_id
+#   sku_name = "standard"
+# }
 
-resource "azurerm_key_vault_secret" "example_secret" {
-  name         = "my-secret-name"
-  value        = "my-super-secret-value"
-  key_vault_id = azurerm_key_vault.kv.id
+# resource "azurerm_key_vault_secret" "example_secret" {
+#   name         = "my-secret-name"
+#   value        = "my-super-secret-value"
+#   key_vault_id = azurerm_key_vault.kv.id
 
-  // Optional attributes
-  content_type = "example-content-type"  # E.g., "password", "API key", etc.
-  tags = {
-    environment = "dev"
-  }
+#   // Optional attributes
+#   content_type = "example-content-type"  # E.g., "password", "API key", etc.
+#   tags = {
+#     environment = "dev"
+#   }
 
-  depends_on = [ azurerm_key_vault_access_policy.access_policy ]
-}
+#   depends_on = [ azurerm_key_vault_access_policy.access_policy ]
+# }
 
-resource "azurerm_key_vault_access_policy" "access_policy_apim" {
-  key_vault_id = azurerm_key_vault.kv.id
-  tenant_id    = data.azurerm_client_config.current.tenant_id
-  object_id     = azurerm_api_management.apim.identity[0].principal_id
-  secret_permissions = ["Get", "Set"]
+# resource "azurerm_key_vault_access_policy" "access_policy_apim" {
+#   key_vault_id = azurerm_key_vault.kv.id
+#   tenant_id    = data.azurerm_client_config.current.tenant_id
+#   object_id     = azurerm_api_management.apim.identity[0].principal_id
+#   secret_permissions = ["Get", "Set"]
 
-  depends_on = [azurerm_api_management.apim]
-}
+#   depends_on = [azurerm_api_management.apim]
+# }
 
-// adding myself to read and create secrets
-resource "azurerm_key_vault_access_policy" "access_policy" {
-  key_vault_id = azurerm_key_vault.kv.id
-  tenant_id    = data.azurerm_client_config.current.tenant_id
-  object_id = data.azurerm_client_config.current.object_id
+# // adding myself to read and create secrets
+# resource "azurerm_key_vault_access_policy" "access_policy" {
+#   key_vault_id = azurerm_key_vault.kv.id
+#   tenant_id    = data.azurerm_client_config.current.tenant_id
+#   object_id = data.azurerm_client_config.current.object_id
 
-  secret_permissions = ["Get", "Set", "List"]
-}
+#   secret_permissions = ["Get", "Set", "List"]
+# }
