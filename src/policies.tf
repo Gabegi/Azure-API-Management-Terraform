49f502ba13f2df0fcf6ac73d599f79a5759d13b0
Gabegi <gabriel_pirastru@hotmail.fr>
2025-01-24 09:18:32 +0100

    changing policy

--- src/policies.tf
+++ src/policies.tf
@@ -1,7 +1,7 @@
-resource "azurerm_api_management_api_policy" "policy" {
-  api_name            = azurerm_api_management_api.api1.name
-  api_management_name = azurerm_api_management.apim.name
-  resource_group_name = azurerm_resource_group.rg.name
+# resource "azurerm_api_management_api_policy" "policy" {
+#   api_name            = azurerm_api_management_api.api1.name
+#   api_management_name = azurerm_api_management.apim.name
+#   resource_group_name = azurerm_resource_group.rg.name
 
-  xml_content = file("./policies/policy1.xml")
-}
+#   xml_content = file("./policies/policy1.xml")
+# }
