4337b1eea3c9a42ba72d77be9199e19cdd09131d
Gabegi <gabriel_pirastru@hotmail.fr>
2025-01-27 09:37:25 +0100

    adding diagnostics

--- src/logging.tf
+++ src/logging.tf
@@ -1,66 +1,66 @@
-# resource "azurerm_application_insights" "app_insights" {
-#   name                = "example-appinsights"
-#   location            = azurerm_resource_group.rg.location
-#   resource_group_name = azurerm_resource_group.rg.name
-#   application_type    = "web"
-# }
+resource "azurerm_application_insights" "app_insights" {
+  name                = "example-appinsights"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  application_type    = "web"
+}
 
-# resource "azurerm_api_management_logger" "logger" {
-#   name                = "example-apimlogger"
-#   api_management_name = azurerm_api_management.apim.name
-#   resource_group_name = azurerm_resource_group.rg.name
+resource "azurerm_api_management_logger" "logger" {
+  name                = "example-apimlogger"
+  api_management_name = azurerm_api_management.apim.name
+  resource_group_name = azurerm_resource_group.rg.name
 
-#   application_insights {
-#     instrumentation_key = azurerm_application_insights.app_insights.instrumentation_key
-#   }
-# }
+  application_insights {
+    instrumentation_key = azurerm_application_insights.app_insights.instrumentation_key
+  }
+}
 
 
-# resource "azurerm_api_management_diagnostic" "example" {
-#   identifier               = "applicationinsights"
-#   resource_group_name      = azurerm_resource_group.rg.name
-#   api_management_name      = azurerm_api_management.apim.name
-#   api_management_logger_id = azurerm_api_management_logger.logger.id
+resource "azurerm_api_management_diagnostic" "example" {
+  identifier               = "applicationinsights"
+  resource_group_name      = azurerm_resource_group.rg.name
+  api_management_name      = azurerm_api_management.apim.name
+  api_management_logger_id = azurerm_api_management_logger.logger.id
 
-#   sampling_percentage       = 5.0
-#   always_log_errors         = true
-#   log_client_ip             = true
-#   verbosity                 = "verbose"
-#   http_correlation_protocol = "W3C"
+  sampling_percentage       = 5.0
+  always_log_errors         = true
+  log_client_ip             = true
+  verbosity                 = "verbose"
+  http_correlation_protocol = "W3C"
 
-#   frontend_request {
-#     body_bytes = 32
-#     headers_to_log = [
-#       "content-type",
-#       "accept",
-#       "origin",
-#     ]
-#   }
+  frontend_request {
+    body_bytes = 32
+    headers_to_log = [
+      "content-type",
+      "accept",
+      "origin",
+    ]
+  }
 
-#   frontend_response {
-#     body_bytes = 32
-#     headers_to_log = [
-#       "content-type",
-#       "content-length",
-#       "origin",
-#     ]
-#   }
+  frontend_response {
+    body_bytes = 32
+    headers_to_log = [
+      "content-type",
+      "content-length",
+      "origin",
+    ]
+  }
 
-#   backend_request {
-#     body_bytes = 32
-#     headers_to_log = [
-#       "content-type",
-#       "accept",
-#       "origin",
-#     ]
-#   }
+  backend_request {
+    body_bytes = 32
+    headers_to_log = [
+      "content-type",
+      "accept",
+      "origin",
+    ]
+  }
 
-#   backend_response {
-#     body_bytes = 32
-#     headers_to_log = [
-#       "content-type",
-#       "content-length",
-#       "origin",
-#     ]
-#   }
-# }
+  backend_response {
+    body_bytes = 32
+    headers_to_log = [
+      "content-type",
+      "content-length",
+      "origin",
+    ]
+  }
+}
